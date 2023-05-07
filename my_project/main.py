f235d7ec494990a09e01058d3e338af2c80079f7
yuanhaibo <yuanhaibo0503@163.com>
2023-05-07 20:33:26 +0800

    @danty 更改路径问题

--- my_project/main.py
+++ my_project/main.py
@@ -91,8 +91,8 @@ def boat():
         name = urllib.parse.unquote(name)
         config = IniFileEditor().get_value("boat","boat_name_excel")
         boat_name = GetExcelValue(name).get_boat_name(config)[0]
-        Image =os.path.abspath(__file__).split("/main.py")[0]+"/my_html/templates/storage_boat/"+boat_name+".png"
-        image = Image.open(Image)
+
+        image = Image.open(os.path.abspath(__file__).split("/main.py")[0]+"/my_html/templates/storage_boat/"+boat_name+".png")
     except Exception as e:
         return abort(400, description=str(e))
     buffer = BytesIO()
